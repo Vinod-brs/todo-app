902cf7636a75d0556218b59d50eb20a32c6b8697
Vinod-brs <kingvinodkumart@gmail.com>
2024-12-26 15:38:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+//render css files
+app.use(express.static("public"));
+
+//placeholders for added task
+var task = ["Get Started", "Finish JQuery", "Practise Nodejs"];
+//placeholders for removed task
+var complete = ["Review JS concepts"];
+
+//post route for adding new task
+app.post("/addtask", function(req, res) {
+    var newTask = req.body.newtask;
+    //add the new task from the post route
+    if(newTask.trim())
+    task.push(newTask);
+    res.redirect("/");
+});
+
+
+
+app.post("/removetask", function(req, res) {
+    var completeTask = req.body.check;
+    //check for the "typeof" the different completed task, then add into the complete task
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        //check if the completed task already exits in the task when checked, then remove it
+        task.splice(task.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (var i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    res.redirect("/");
+});
+
+app.post("/adtask", function(req, res) {
+    var addTask = req.query.t;
+
+        if(complete.includes(addTask)){
+            let index = complete.indexOf(addTask);
+            complete.splice(index, 1);
+
+            task.push(addTask)
+        }
+    res.redirect("/");
+});
+
+app.post("/dtask", function(req, res) {
+    var dTask = req.query.delete;
+            
+        if(complete.includes(dTask)){
+            let index = complete.indexOf(dTask);
+            complete.splice(index, 1);
+        }
+    res.redirect("/");
+});
+
+//render the ejs and display added task, completed task
+app.get("/", function(req, res) {
+    res.render("index", { task: task, complete: complete });
+});
+//set app to listen on port 4002
+app.listen(port, function() {
+    console.log("server is running on port " + port);
+});
+
+app.get('*', (req, res) => {
+    res.render("index", { task: task, complete: complete });
+ })
+
